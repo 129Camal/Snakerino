d36ee96c48a9f87fa1a25c424db2e9441c930661
129Camal <frederico21pinto@hotmail.com>
2018-07-27 18:49:31 +0100

    Ainda pode levar uns upgrades! :bomb:

--- snakerino.py
+++ snakerino.py
@@ -1,6 +1,5 @@
 import curses
 import random
-import time
 
 stdscr = curses.initscr()
 key = curses.KEY_RIGHT
@@ -14,8 +13,7 @@ screen.border(0)
 screen.nodelay(1)
 screen.keypad(True)
 
-
-screen.addstr(0, 2, 'Score : ' + str(score) + ' ')  # Printing 'Score' and
+screen.addstr(0, 2, 'Score : ' + str(score) + ' ')
 screen.addstr(0, 27, ' SNAKErino by Camal')
 
 snake_x = x / 4
@@ -27,7 +25,7 @@ snake = [
     [snake_y, snake_x - 2]
 ]
 
-screen.timeout(150 - (len(snake)/5 + len(snake)/10)%120)
+screen.timeout(150 - (len(snake) / 5 + len(snake) / 10) % 120)
 
 food = [y / 2, x / 2]
 
@@ -42,12 +40,13 @@ while True:
 
     if snake[0][0] in [1, y - 2] or snake[0][1] in [1, x - 2] or snake[0] in snake[1:]:
 
-        stdscr.addstr(y / 2, x / 8, 'YOU LOSS! Press R to Retry or another key to Quit, then press Enter!')
+        screen.erase()
+        stdscr.addstr(y / 2, x / 10, 'YOU LOSS! Press R to Retry or another key to Quit, then press Enter!')
+        screen.erase()
 
         desire = stdscr.getstr(3 * y / 4, x / 2)
 
         if desire == 'r' or desire == 'R':
-            pass
             snake = [
                 [snake_y, snake_x],
                 [snake_y, snake_x - 1],
@@ -55,7 +54,6 @@ while True:
             ]
             score = 0
             key = curses.KEY_RIGHT
-            screen.erase()
             screen.border(0)
             screen.addch(y / 2, x / 2, '*')
             screen.addstr(0, 2, 'Score : ' + str(score) + ' ')
@@ -85,6 +83,7 @@ while True:
         if snake[0] == food:
             food = None
             score += 1
+
             screen.addstr(0, 2, 'Score : ' + str(score) + ' ')
 
             while food is None:
@@ -92,11 +91,13 @@ while True:
                     random.randint(2, y - 3),
                     random.randint(1, x - 2)
                 ]
+
                 food = new_food if new_food not in snake else None
             screen.addch(food[0], food[1], '*')
 
         else:
             tail = snake.pop()
+
             screen.addch(tail[0], tail[1], ' ')
 
         screen.addch(snake[0][0], snake[0][1], '#')
